ea05a12e5875bceec4a4dda832a8040fff6a7220
QuanZzz <zhouquaneric@gmail.com>
2024-01-23 11:43:35 -0500

    added event handlers into the component

--- src/components/AutoCompleteInput.js
+++ src/components/AutoCompleteInput.js
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import { userNames } from "@/utils/userNames";
+import cx from "classnames";
 
 export const AutoCompleteInput = () => {
   const [inputValue, setInputValue] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [highlightedIndex, setHighlightedIndex] = useState(-1);
+  const suggestionRefs = useRef([]);
 
   useEffect(() => {
-    if (!inputValue) {
+    if (!inputValue || !showSuggestions) {
       setSuggestions([]);
       return;
     }
@@ -22,7 +26,41 @@ export const AutoCompleteInput = () => {
     return () => {
       clearTimeout(handler);
     };
-  }, [inputValue]);
+  }, [inputValue, showSuggestions]);
+
+  useEffect(() => {
+    if (highlightedIndex > -1 && highlightedIndex < suggestions.length) {
+      suggestionRefs.current[highlightedIndex]?.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+      });
+    }
+  }, [highlightedIndex, suggestions.length]);
+
+  const handleSuggestonClick = (suggestion) => {
+    setInputValue(suggestion);
+    setSuggestions([]);
+    setShowSuggestions(false);
+  };
+
+  const handleOnChange = (e) => {
+    setInputValue(e.target.value);
+    setShowSuggestions(true);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowDown") {
+      setHighlightedIndex((prevIndex) =>
+        prevIndex < suggestions.length - 1 ? prevIndex + 1 : prevIndex
+      );
+    } else if (e.key === "ArrowUp") {
+      setHighlightedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
+    } else if (e.key === "Enter" && highlightedIndex > -1) {
+      setInputValue(suggestions[highlightedIndex]);
+      setSuggestions([]);
+      setShowSuggestions(false);
+    }
+  };
 
   return (
     <div className="relative">
@@ -33,14 +71,25 @@ export const AutoCompleteInput = () => {
           placeholder="Search for a username"
           className="input border border-gray-300 text-base text-gray-900 focus:outline-none focus:border-blue h-38 w-full"
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={handleOnChange}
+          onKeyDown={handleKeyDown}
         />
       </div>
-      {!!suggestions?.length && (
+      {!!suggestions?.length && showSuggestions && (
         <div className="absolute bg-white top-10 left-0 right-0 border border-gray-400 rounded-md py-4 px-4 text-left mx-2 z-10">
           {suggestions.map((suggestion, index) => (
-            <div role="button" tabIndex={0} key={index}>
-              <p className="cursor-pointer text-gray-400 my-3 text-base">
+            <div
+              role="button"
+              tabIndex={0}
+              key={index}
+              ref={(el) => (suggestionRefs.current[index] = el)}
+              onClick={() => handleSuggestonClick(suggestion)}
+            >
+              <p
+                className={cx("cursor-pointer text-gray-400 my-3 text-base", {
+                  "bg-blue-200": index === highlightedIndex,
+                })}
+              >
                 {suggestion}
               </p>
             </div>
